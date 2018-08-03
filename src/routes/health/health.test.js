f2b39bf20a6b60772fd80a87be167d5dac0b44ce
Cheyenne Arrowsmith <cheyenne.arrowsmith@ontario.ca>
2018-08-02 20:18:44 -0400

    health test back in the code

--- src/routes/health/health.test.js
+++ src/routes/health/health.test.js
@@ -2,8 +2,8 @@ const request = require("supertest");
 
 const { healthRouter } = require("./health.router");
 
-// describe("tests for the health check endpoint", () => {
-//   it("returns a 200", () => {
-//     expect(true).toBe(true);
-//   });
-// });
+describe("tests for the health check endpoint", () => {
+  it("returns a 200", () => {
+    expect(true).toBe(true);
+  });
+});
